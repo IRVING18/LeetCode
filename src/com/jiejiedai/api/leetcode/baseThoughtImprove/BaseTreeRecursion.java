61aba16e9532adbe94a4e680248575e59ac43ddd
IRVING18 <1141502381@qq.com>
2023-01-09 13:41:44 +0800

    树与递归

--- src/com/jiejiedai/api/leetcode/baseThoughtImprove/BaseTreeRecursion.java
+++ src/com/jiejiedai/api/leetcode/baseThoughtImprove/BaseTreeRecursion.java
@@ -6,6 +6,10 @@ import java.util.*;
 
 /**
  * 树与递归
+ * <p>
+ * 树的操作：
+ * 1、利用Queue队列
+ * 2、递归
  */
 public class BaseTreeRecursion {
 
@@ -56,30 +60,29 @@ public class BaseTreeRecursion {
 
     /**
      * 剑指 Offer 32 - II. 从上到下打印二叉树 II
-     *
+     * <p>
      * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
-     *
+     * <p>
      * 例如:
      * 给定二叉树: [3,9,20,null,null,15,7],
-     *
-     *     3
-     *    / \
-     *   9  20
-     *     /  \
-     *    15   7
+     * <p>
+     * 3
+     * / \
+     * 9  20
+     * /  \
+     * 15   7
      * 返回其层次遍历结果：
-     *
+     * <p>
      * [
-     *   [3],
-     *   [9,20],
-     *   [15,7]
+     * [3],
+     * [9,20],
+     * [15,7]
      * ]
-     *
+     * <p>
      * 链接：https://leetcode.cn/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof
-     *
+     * <p>
      * 思路一：创建Pair，存储level和node
      * 思路二：遍历当前level，
-     *
      */
     public List<List<Integer>> levelOrder2(TreeNode root) {
 //        if (root == null){
@@ -128,10 +131,11 @@ public class BaseTreeRecursion {
 
     }
 
-    class Pair{
+    class Pair {
         TreeNode node;
         int level;
-        Pair(TreeNode node,int level){
+
+        Pair(TreeNode node, int level) {
             this.node = node;
             this.level = level;
         }
@@ -139,33 +143,33 @@ public class BaseTreeRecursion {
 
     /**
      * 剑指 Offer 32 - III. 从上到下打印二叉树 III
-     *
+     * <p>
      * 请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
-     *
+     * <p>
      * 例如:
      * 给定二叉树: [3,9,20,null,null,15,7],
-     *
-     *     3
-     *    / \
-     *   9  20
-     *     /  \
-     *    15   7
+     * <p>
+     * 3
+     * / \
+     * 9  20
+     * /  \
+     * 15   7
      * 返回其层次遍历结果：
-     *
+     * <p>
      * [
-     *   [3],
-     *   [20,9],
-     *   [15,7]
+     * [3],
+     * [20,9],
+     * [15,7]
      * ]
-     *
+     * <p>
      * 链接：https://leetcode.cn/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof
-     *
+     * <p>
      * 思路：
      * 沿用上边 遍历当前level，
      * 在每个level层级，处理时，偶数行，不用list，改用一个队列，offerLast()。
      */
     public List<List<Integer>> levelOrder3(TreeNode root) {
-        if (root == null){
+        if (root == null) {
             return null;
         }
         List<List<Integer>> res = new ArrayList<>();
@@ -196,4 +200,172 @@ public class BaseTreeRecursion {
         return res;
     }
 
+    /**
+     * 剑指 Offer 26. 树的子结构
+     * <p>
+     * 输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+     * <p>
+     * B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+     * <p>
+     * 例如:
+     * 给定的树 A:
+     * <p>
+     *      3
+     *     / \
+     *    4   5
+     *   / \
+     *  1   2
+     * 给定的树 B：
+     * <p>
+     *    4 
+     *   /
+     *  1
+     * 返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：A = [1,2,3], B = [3,1]
+     * 输出：false
+     * 示例 2：
+     * <p>
+     * 输入：A = [3,4,5,1,2], B = [4,1]
+     * 输出：true
+     * <p>
+     * 链接：https://leetcode.cn/problems/shu-de-zi-jie-gou-lcof
+     * <p>
+     * 思路：递归
+     * 1、第一层递归：判断A是否和B完全相交，iscontain()
+     * 2、第二层递归：判断A是否和B完全相交，不完全相交-> left、right是否完全相交 isSubStructure()
+     */
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if (A == null || B == null) {
+            return false;
+        }
+
+        return isContain(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
+    }
+
+    /**
+     * 是否完全相交
+     * <p>
+     * 或者B完全在A中
+     */
+    private boolean isContain(TreeNode A, TreeNode B) {
+        //B完全在A中
+        if (B == null) {
+            return true;
+        }
+
+        return A != null && A.val == B.val && isContain(A.left, B.left) && isContain(A.right, B.right);
+    }
+
+    /**
+     * 剑指 Offer 27. 二叉树的镜像
+     * 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+     * <p>
+     * 例如输入：
+     * <p>
+     *      4
+     *    /   \
+     *   2     7
+     *  / \   / \
+     * 1   3 6   9
+     * 镜像输出：
+     * <p>
+     *      4
+     *    /   \
+     *   7     2
+     *  / \   / \
+     * 9   6 3   1
+     * <p>
+     *  
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：root = [4,2,7,1,3,6,9]
+     * 输出：[4,7,2,9,6,3,1]
+     * <p>
+     * 链接：https://leetcode.cn/problems/er-cha-shu-de-jing-xiang-lcof
+     */
+    public static TreeNode mirrorTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = null;
+        if (root.left != null) {
+            left = root.left;
+        }
+        TreeNode right = null;
+        if (root.right != null) {
+            right = root.right;
+        }
+
+        root.left = right;
+        root.right = left;
+
+        mirrorTree(root.left);
+        mirrorTree(root.right);
+
+        return root;
+    }
+
+
+    /**
+     * 剑指 Offer 28. 对称的二叉树
+     * <p>
+     * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+     * <p>
+     * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+     * <p>
+     *     1
+     *    / \
+     *   2   2
+     *  / \ / \
+     * 3  4 4  3
+     * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+     * <p>
+     *     1
+     *    / \
+     *   2   2
+     *    \   \
+     *    3    3
+     * <p>
+     *  
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：root = [1,2,2,3,4,4,3]
+     * 输出：true
+     * 示例 2：
+     * <p>
+     * 输入：root = [1,2,2,null,3,null,3]
+     * 输出：false
+     * <p>
+     * 链接：https://leetcode.cn/problems/dui-cheng-de-er-cha-shu-lcof
+     */
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmertric(root, root);
+    }
+
+    public boolean isSymmertric(TreeNode nodeA, TreeNode nodeB) {
+        if (nodeA == null && nodeB == null) {
+            return true;
+        }
+        if (nodeA == null || nodeB == null) {
+            return false;
+        }
+        return nodeA.val == nodeB.val && isSymmertric(nodeA.left, nodeB.right) && isSymmertric(nodeA.right, nodeB.left);
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(1);
+        TreeNode right = new TreeNode(2);
+        node.left = null;
+        node.right = right;
+        System.out.println(node.val + " " + (node.left != null ? node.left.val : "null") + " " + node.right.val);
+
+        mirrorTree(node);
+        System.out.println(node.val + " " + (node.left != null ? node.left.val : "null") + " " + (node.right != null ? node.right.val : "null"));
+    }
 }
